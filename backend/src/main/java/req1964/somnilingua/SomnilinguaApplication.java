965a620d2e0e03a55fafdebc418b5a07b6a8261a
ReQ1964 <slomianykacper4@gmail.com>
2026-04-22 17:11:35 +0200

    init be

---
+++ backend/src/main/java/req1964/somnilingua/SomnilinguaApplication.java
@@ -0,0 +1,13 @@
+package req1964.somnilingua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomnilinguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomnilinguaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/req1964/somnilingua/SomnilinguaApplicationTests.java
@@ -0,0 +1,13 @@
+package req1964.somnilingua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomnilinguaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
