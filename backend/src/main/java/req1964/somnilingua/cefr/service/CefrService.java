340f9684dd60e4879d3fb846f2c1ba3a5401bd7f
ReQ1964 <slomianykacper4@gmail.com>
2026-06-09 17:56:03 +0200

    fix cefr order

--- backend/src/main/java/req1964/somnilingua/cefr/service/CefrService.java
+++ backend/src/main/java/req1964/somnilingua/cefr/service/CefrService.java
@@ -7,6 +7,7 @@ import req1964.somnilingua.cefr.dto.CefrResponse;
 import req1964.somnilingua.cefr.mapper.CefrMapper;
 import req1964.somnilingua.cefr.repository.CefrRepository;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Transactional
@@ -15,11 +16,10 @@ import java.util.List;
 public class CefrService {
   private final CefrRepository cefrRepository;
 
-  private static final Long DEV_USER_ID = 1L;
-
   public List<CefrResponse> getAllCefrs() {
     return cefrRepository.findAll().stream()
         .map(CefrMapper::toResponse)
+        .sorted(Comparator.comparingInt(CefrResponse::getOrder))
         .toList();
   }
 
