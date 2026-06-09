b36d4b526dbbe59751824d21e89b1774454fa56e
ReQ1964 <slomianykacper4@gmail.com>
2026-06-09 17:50:45 +0200

    cefr controller swagger

--- backend/src/main/java/req1964/somnilingua/cefr/controller/CefrController.java
+++ backend/src/main/java/req1964/somnilingua/cefr/controller/CefrController.java
@@ -1,5 +1,7 @@
 package req1964.somnilingua.cefr.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/cefrs")
 @RequiredArgsConstructor
+@Tag(name = "CEFR Controller", description = "Operations related to CEFR levels")
 public class CefrController {
+
   private final CefrService cefrService;
 
   @GetMapping
-  ResponseEntity<List<CefrResponse>> getAllCefrs() {
-    return ResponseEntity.ok().body(cefrService.getAllCefrs());
+  @Operation(
+      summary = "Get all CEFR levels",
+      description = "Returns a list of all CEFR entries available in the system"
+  )
+  public ResponseEntity<List<CefrResponse>> getAllCefrs() {
+    return ResponseEntity.ok(cefrService.getAllCefrs());
   }
-
 }
